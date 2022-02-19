1a3a22601aed075a1914071bc851ac4906ee9329
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-02-19 16:08:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #删除自带argon
-rm -rf  package/lean/luci-theme-argon
+rm -rf  feeds/luci/themes/luci-theme-argon
 
 #添加老竭力的argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
