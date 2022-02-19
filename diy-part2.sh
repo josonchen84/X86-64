3b273af4b5b5fc8a7a99ff93a04909a16fee3d79
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-02-19 16:07:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,3 +33,9 @@ sed -i 's/R22.2.2/For FMJYY23/g' package/lean/default-settings/files/zzz-default
 
 #### 64位5.10内核切换5.15
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+
+sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透"/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
+
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
+
+#sed -i 's/"CPU 性能优化调节"/"处理器性能优化调节"/g' feeds/luci/applications/luci-app-cpufreq/po/zh-cn/cpufreq.po
