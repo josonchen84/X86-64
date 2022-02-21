f01248aea54e98a8f5f8a023807b29ae1e27d54e
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-02-21 12:56:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ rm -rf  feeds/luci/themes/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-#### 修改主机名字
+#### 修改主机名
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='X86'' package/lean/default-settings/files/zzz-default-settings
 
 #### 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
