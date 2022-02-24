5a5e32f4bc2d71830212453a71dedf8d1653c0c7
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-02-24 10:20:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
