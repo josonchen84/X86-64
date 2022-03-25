ce16bcd762431a00f4312d78d338e06fcf7162df
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-25 16:01:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,4 +30,3 @@ sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透设置"/g' feeds/luci/a
 
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
