846f6c22e1012968a4abc3a9b46862d2efe48ba0
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-25 16:00:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,10 @@
 
 # Add a feed source
 
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 sed -i '$a src-git https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic' feeds.conf.default
