3edc1767c2fc299ecfd75c216a59c2937a89b693
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-03-25 22:53:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,13 +17,10 @@ rm -rf  feeds/luci/themes/luci-theme-argon
 
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-#### 修改主机名
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='X86'' package/lean/default-settings/files/zzz-default-settings
 
-#### 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
-#### 稳定版修改R21xxx+自己的名字
 sed -i 's/R22.3.13/For FMJYY23/g' package/lean/default-settings/files/zzz-default-settings
 
 sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透设置"/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
